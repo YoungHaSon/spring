5778fc8d5ec72c2b3fdf461f04b218c1e6b7a209
YoungHaSon <thsdudgk123@gmail.com>
2019-06-20 13:31:12 +0900

    spring ioc bean life cycle

--- src/main/java/kr/or/ddit/ioc/placeholder/DbInfo.java
+++ src/main/java/kr/or/ddit/ioc/placeholder/DbInfo.java
@@ -1,10 +1,21 @@
 package kr.or.ddit.ioc.placeholder;
 
-public class DbInfo {
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.BeansException;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.ApplicationContextAware;
+
+import kr.or.ddit.board.dao.BoardDao;
+import kr.or.ddit.board.dao.IboardDao;
+
+public class DbInfo implements ApplicationContextAware{
 	private String driver;
 	private String url;
 	private String username;
 	private String password;
+
+	private static final Logger logger = LoggerFactory.getLogger(DbInfo.class);
 	
 	//생성자 주입?
 	public DbInfo(String driver, String url, String username, String password) {
@@ -30,6 +41,18 @@ public class DbInfo {
 
 	public String getPassword() {
 		return password;
+	}
+
+	@Override
+	public void setApplicationContext(ApplicationContext applicationContext) throws BeansException {
+		//형변환 없이 받을쑤 있다! , BoardDao.class 요렇게 붙여주면
+		IboardDao boardDao = applicationContext.getBean("boardDao", BoardDao.class);
+		String msg = boardDao.sayHello();
+		logger.debug("msg : {}", msg);
+	}
+	
+	public void init() {
+		logger.debug("init : {}");
 	}
 	
 }
---
+++ src/test/java/kr/or/ddit/ioc/SpringlifeCycleTest.java
@@ -0,0 +1,41 @@
+package kr.or.ddit.ioc;
+
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertNotNull;
+
+import javax.annotation.Resource;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.test.context.ContextConfiguration;
+import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+
+import kr.or.ddit.ioc.placeholder.DbInfo;
+
+//스프링 컨테이너 구성? SpringJUnit4ClassRunner.class 쓰이는 모든 클래스가 컨테이너?
+@RunWith(SpringJUnit4ClassRunner.class)
+@ContextConfiguration("classpath:kr/or/ddit/ioc/application-ioc-placeholder.xml")
+public class SpringlifeCycleTest {
+	
+	@Resource(name = "dbinfo")
+	private DbInfo dbInfo;
+
+	@Test
+	public void lifeCycleTest() {
+		/*** Given ***/
+		/*** When ***/
+
+		/*** Then ***/
+		assertNotNull(dbInfo.getDriver());
+		assertNotNull(dbInfo.getUrl());
+		assertNotNull(dbInfo.getUsername());
+		assertNotNull(dbInfo.getPassword());
+		
+		assertEquals("oracle.jdbc.driver.OracleDriver", dbInfo.getDriver());
+		assertEquals("jdbc:oracle:thin:@localhost:1521:xe", dbInfo.getUrl());
+		assertEquals("pc13", dbInfo.getUsername());
+		assertEquals("java", dbInfo.getPassword());
+
+	}
+
+}
