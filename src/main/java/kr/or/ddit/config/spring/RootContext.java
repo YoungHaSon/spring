b8c51cc7acc3e3afccb1d6e44dd9d76bce68458d
YoungHaSon <thsdudgk123@gmail.com>
2019-07-08 10:29:08 +0900

    Spring batch  - 문자열 처리 배치rangerBatch   rangerBatch   rangerBatchTest   scheduler 등록   rangerBatchScheduler 등록

--- src/main/java/kr/or/ddit/config/spring/RootContext.java
+++ src/main/java/kr/or/ddit/config/spring/RootContext.java
@@ -4,11 +4,17 @@ import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.ComponentScan.Filter;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.FilterType;
+import org.springframework.context.annotation.ImportResource;
 import org.springframework.stereotype.Repository;
 import org.springframework.stereotype.Service;
 
 //service, repository 두 @을 대상으로 스캔하겠다
-@Configuration			//설정파일이라는 표시!					//요거 중요! 안하면 bean이 여러개 생김
+@Configuration	
+@ImportResource({
+				"classpath:kr/or/ddit/config/spring/application-scheduler.xml",
+				"classpath:kr/or/ddit/config/spring/application-batch.xml"
+				})
+		//설정파일이라는 표시!					//요거 중요! 안하면 bean이 여러개 생김
 @ComponentScan(basePackages = "kr.or.ddit", useDefaultFilters = false
 						, includeFilters = {@Filter(type = FilterType.ANNOTATION, classes = {Service.class , Repository.class})})
 public class RootContext {
